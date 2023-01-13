c6a289e7f22142d7189ffd515b17ddf2cac2d6d3
Volodymyr Batsyk <volodymyrbatsyk@MacBook-Air-Volodymyr.local>
2023-01-13 23:03:12 +0200

    rm node_modules

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
 
-const token = '5865200994:AAE0Mku1UEcT7tI_-Q7uvPmGHwFc1d-buJQ'
-const inspectorUrl = 'https://63c088d245aeb504615c544c--fantastic-pegasus-31850b.netlify.app/'
+const token = "5865200994:AAE0Mku1UEcT7tI_-Q7uvPmGHwFc1d-buJQ";
+const inspectorUrl = "https://fantastic-pegasus-31850b.netlify.app/";
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
 
-  bot.on('message', async(msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text
+  if (text === "/start") {
+    await bot.sendMessage(chatId, "Hello", {
+      reply_markup: {
+        keyboard: [
+          [
+            {
+              text: "Переглянути фотофіксацію",
+              web_app: { url: inspectorUrl },
+            },
+            { text: "Оплатити" },
+          ],
+        ],
+      },
+    });
 
-    if(text === '/start') {
-        await bot.sendMessage(chatId,'Hello', {
-            reply_markup: {
-                keyboard: [
-                    [{text: 'Переглянути фотофіксацію', web_app: {url: inspectorUrl}}, {text: 'Оплатити'}]
-                ]
-            }
-        })
-    }
-  
-  });
+    await bot.sendMessage(chatId, "Inline key", {
+      reply_markup: {
+        inline_keyboard: [[{ text: "Inline", web_app: { url: inspectorUrl } }]],
+      },
+    });
+  }
+});
